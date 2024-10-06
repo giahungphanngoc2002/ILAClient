3b2650f89c17717109a799a8d5792bab514e8e09
unknown <giahungphanngoc2002@gmail.com>
2024-10-07 00:02:34 +0700

    Student Table 7/10

--- src/pages/Teacher/StudentTable.jsx
+++ src/pages/Teacher/StudentTable.jsx
@@ -6,18 +6,20 @@ import { FaArrowLeft } from 'react-icons/fa';
 
 const StudentTable = ({ idClass, handleBackSchedule }) => {
   const initialUsers = [
-    { id: 1, name: 'Michael Holz', date: '04/10/2013', role: 'Admin', status: true, avatar: '/images/trainer-1.jpg', oralScore: [8.5, 6], quizScore: [7, 9.8], testScore: [9], finalScore: 8.5 },
-    { id: 2, name: 'Paula Wilson', date: '05/08/2014', role: 'Publisher', status: true, avatar: '/images/trainer-2.jpg', oralScore: [7], quizScore: [6.5], testScore: [8], finalScore: 7.5 },
-    { id: 3, name: 'Antonio Moreno', date: '11/05/2015', role: 'Publisher', status: true, avatar: '/images/trainer-3.jpg', oralScore: [9, 5.8], quizScore: [8], testScore: [9.5, 10], finalScore: 9.2 },
-    { id: 4, name: 'Mary Saveley', date: '06/09/2016', role: 'Reviewer', status: true, avatar: '/images/trainer-4.jpg', oralScore: [6.5], quizScore: [7], testScore: [8], finalScore: 7.8 },
+    { id: 1, name: 'Michael Holz', date: '04/10/2013', role: 'Admin', avatar: '/images/trainer-1.jpg', oralScore: [8.5, 6], quizScore: [7, 9.8], testScore: [9], finalScore: 8.5 },
+    { id: 2, name: 'Paula Wilson', date: '05/08/2014', role: 'Publisher', avatar: '/images/trainer-2.jpg', oralScore: [7], quizScore: [6.5], testScore: [8], finalScore: 7.5 },
+    { id: 3, name: 'Antonio Moreno', date: '11/05/2015', role: 'Publisher', avatar: '/images/trainer-3.jpg', oralScore: [9, 5.8], quizScore: [8], testScore: [9.5, 10], finalScore: 9.2 },
+    { id: 4, name: 'Mary Saveley', date: '06/09/2016', role: 'Reviewer', avatar: '/images/trainer-4.jpg', oralScore: [6.5], quizScore: [7], testScore: [8], finalScore: 7.8 },
   ];
 
-  console.log(idClass)
-
-  const [users, setUsers] = useState(initialUsers);
+  // Thêm trạng thái status tạm thời
+  const [users, setUsers] = useState(
+    initialUsers.map(user => ({ ...user, status: false })) // mặc định là false
+  );
   const [showModal, setShowModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
 
+  // Hàm toggle status của từng user
   const toggleStatus = (id) => {
     const updatedUsers = users.map(user =>
       user.id === id ? { ...user, status: !user.status } : user
@@ -25,9 +27,13 @@ const StudentTable = ({ idClass, handleBackSchedule }) => {
     setUsers(updatedUsers);
   };
 
+  // Hàm lưu user và trả về danh sách true/false cho từng người
   const saveUsers = () => {
-    const userAbsent = users.filter(user => user.status === false);
-    console.log(userAbsent);
+    const absentUserIds = users
+      .filter(user => user.status === false)  // Lọc những user có status là false
+      .map(user => user.id);                  // Lấy ra id của những user đó
+
+    console.log(absentUserIds);  // Trả về danh sách những student id có status là false
   };
 
   const openModal = (user) => {
@@ -108,130 +114,7 @@ const StudentTable = ({ idClass, handleBackSchedule }) => {
       {selectedUser && (
         <Modal show={showModal} onHide={closeModal} size="xl">
           <Modal.Body>
-            <div className="bg-gray-100 flex justify-center">
-              <div className="bg-white p-8 rounded-lg shadow-lg w-full">
-                <h2 className="text-2xl font-bold mb-4 text-center">Bảng Điểm Học Sinh</h2>
-                <table className="table-auto w-full border-collapse">
-                  <thead>
-                    <tr>
-                      <th className="border px-4 py-2">Tên học sinh</th>
-                      <th className="border px-4 py-2 cursor-pointer"
-                        onClick={() => {
-                          setSelectedUser({
-                            ...selectedUser,
-                            oralScore: [...selectedUser.oralScore, ''],
-                          });
-                        }}>
-                        Điểm miệng
-                      </th>
-                      <th className="border px-4 py-2 cursor-pointer"
-                        onClick={() => {
-                          setSelectedUser({
-                            ...selectedUser,
-                            quizScore: [...selectedUser.quizScore, ''],
-                          });
-                        }}>
-                        Điểm 15 phút
-                      </th>
-                      <th className="border px-4 py-2 cursor-pointer"
-                        onClick={() => {
-                          setSelectedUser({
-                            ...selectedUser,
-                            testScore: [...selectedUser.testScore, ''],
-                          });
-                        }}>
-                        Điểm 1 tiết
-
-                      </th>
-                      <th className="border px-4 py-2">Điểm cuối kì</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td className="border px-4 py-2">
-                        <input
-                          type="text"
-                          name="name"
-                          value={selectedUser.name}
-                          readOnly
-                          className="w-full border rounded px-2 py-1 bg-gray-100"
-                          style={{ width: '250px' }}
-                        />
-                      </td>
-
-                      {/* Điểm miệng */}
-                      <td className="border px-4 py-2">
-                        <div className="grid grid-cols-2 gap-2">
-                          {selectedUser.oralScore.map((score, index) => (
-                            <input
-                              key={index}
-                              type="text"
-                              value={score}
-                              onChange={(e) => {
-                                const newScores = [...selectedUser.oralScore];
-                                newScores[index] = e.target.value;
-                                setSelectedUser({ ...selectedUser, oralScore: newScores });
-                              }}
-                              className="w-full border rounded px-2 py-1"
-                            />
-                          ))}
-                        </div>
-                      </td>
-
-                      {/* Điểm 15 phút */}
-                      <td className="border px-4 py-2">
-                        <div className="grid grid-cols-2 gap-2">
-                          {selectedUser.quizScore.map((score, index) => (
-                            <input
-                              key={index}
-                              type="text"
-                              value={score}
-                              onChange={(e) => {
-                                const newScores = [...selectedUser.quizScore];
-                                newScores[index] = e.target.value;
-                                setSelectedUser({ ...selectedUser, quizScore: newScores });
-                              }}
-                              className="w-full border rounded px-2 py-1"
-                            />
-                          ))}
-                        </div>
-                      </td>
-
-                      {/* Điểm 1 tiết */}
-                      <td className="border px-4 py-2">
-                        <div className="grid grid-cols-2 gap-2">
-                          {selectedUser.testScore.map((score, index) => (
-                            <input
-                              key={index}
-                              type="text"
-                              value={score}
-                              onChange={(e) => {
-                                const newScores = [...selectedUser.testScore];
-                                newScores[index] = e.target.value;
-                                setSelectedUser({ ...selectedUser, testScore: newScores });
-                              }}
-                              className="w-full border rounded px-2 py-1"
-                            />
-                          ))}
-                        </div>
-                      </td>
-
-                      {/* Điểm cuối kì */}
-                      <td className="border px-4 py-2">
-                        <input
-                          type="text"
-                          name="finalScore"
-                          value={selectedUser.finalScore}
-                          onChange={(e) => setSelectedUser({ ...selectedUser, finalScore: e.target.value })}
-                          className="w-full border rounded px-2 py-1"
-                        />
-                      </td>
-                    </tr>
-                  </tbody>
-
-                </table>
-              </div>
-            </div>
+            {/* Nội dung modal */}
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={closeModal}>
@@ -253,7 +136,6 @@ const StudentTable = ({ idClass, handleBackSchedule }) => {
       )}
 
     </div>
-
   );
 };
 
