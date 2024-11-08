bec8e298979a975bf287b4005e2e04b169ee4787
phuc <phuct6776@gmail.com>
2024-11-08 17:07:30 +0700

    update searchuser message and socket fe

--- src/services/ConversationService.js
+++ src/services/ConversationService.js
@@ -7,8 +7,15 @@ const axiosJWT = axios.create();
 export const createConversation = async (data, access_token) => {
   const res = await axiosJWT.post(`${API_URL}/createConversation`, data, {
     headers: {
-      token: `Bearer ${access_token}`,
+      Authorization: `Bearer ${access_token}`,
     },
+  });
+  return res.data;
+};
+
+export const searchUser = async (search) => {
+  const res = await axios.get(`${API_URL}/searchUser`, {
+    params: { search },
   });
   return res.data;
 };
@@ -16,7 +23,7 @@ export const createConversation = async (data, access_token) => {
 export const updateConversation = async (id, data, access_token) => {
   const res = await axiosJWT.put(`${API_URL}/updateConversation/${id}`, data, {
     headers: {
-      token: `Bearer ${access_token}`,
+      Authorization: `Bearer ${access_token}`,
     },
   });
   return res.data;
@@ -25,7 +32,7 @@ export const updateConversation = async (id, data, access_token) => {
 export const getDetailConversation = async (id, access_token) => {
   const res = await axiosJWT.get(`${API_URL}/detailsConversation/${id}`, {
     headers: {
-      token: `Bearer ${access_token}`,
+      Authorization: `Bearer ${access_token}`,
     },
   });
   return res.data;
@@ -34,7 +41,7 @@ export const getDetailConversation = async (id, access_token) => {
 export const deleteConversation = async (id, access_token) => {
   const res = await axiosJWT.delete(`${API_URL}/deleteConversation/${id}`, {
     headers: {
-      token: `Bearer ${access_token}`,
+      Authorization: `Bearer ${access_token}`,
     },
   });
   return res.data;
@@ -43,11 +50,12 @@ export const deleteConversation = async (id, access_token) => {
 export const getAllConversations = async (access_token) => {
   const res = await axiosJWT.get(`${API_URL}/getAllConversations`, {
     headers: {
-      token: `Bearer ${access_token}`,
+      Authorization: `Bearer ${access_token}`,
     },
   });
   return res.data;
 };
+
 export const getAllConversationsForTeacher = async (id) => {
   const res = await axios.get(`${API_URL}/getAllConversationsForTeacher/${id}`);
   return res.data;
