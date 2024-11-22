f638e814e790222ac07a8b09e1c51526c4c245d2
phuc <phuct6776@gmail.com>
2024-11-22 16:20:12 +0700

    update deploy

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,9 @@
-
-
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/user";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://ila-server-2-3bw0.onrender.com/api/user"
+    : "http://localhost:3001/api/user";
 
 export const axiosJWT = axios.create();
 
@@ -41,11 +42,11 @@ export const updateUser = async (id, formData, access_token) => {
   try {
     const res = await axios.put(
       `${API_URL}/updateUser/${id}`,
-      formData, // Chỉ gửi `formData`
+      formData,
       {
         headers: {
-          "Content-Type": "multipart/form-data", // Đảm bảo backend nhận dạng file
-          token: `Bearer ${access_token}`, // Token để xác thực
+          "Content-Type": "multipart/form-data",
+          token: `Bearer ${access_token}`,
         },
       }
     );
@@ -89,38 +90,26 @@ export const resetPassword = async (resetToken, newPassword) => {
 };
 
 export const getAllUser = async () => {
-  const res = await axios.get(`http://localhost:3001/api/user/getAllUser`)
-  return res.data
-}
+  const res = await axios.get(`${API_URL}/getAllUser`);
+  return res.data;
+};
 
 export const getAllTopUser = async () => {
-  const res = await axios.get(`http://localhost:3001/api/user/top`)
-  return res.data
-}
-
-
-
-
+  const res = await axios.get(`${API_URL}/top`);
+  return res.data;
+};
 
 export const createContact = async (userId, contactData) => {
-  const res = await axios.post(`http://localhost:3001/api/user/createContact/${userId}`, contactData);
-
+  const res = await axios.post(`${API_URL}/createContact/${userId}`, contactData);
   return res.data;
-
 };
 
 export const updateContact = async (contactId, updatedData) => {
-
-  const res = await axios.put(`http://localhost:3001/api/user/updateContact/${contactId}`, updatedData);
-
+  const res = await axios.put(`${API_URL}/updateContact/${contactId}`, updatedData);
   return res.data;
-
 };
 
 export const getInfoContactByUserId = async (userId) => {
-
-  const res = await axios.get(`http://localhost:3001/api/user/getdetailsInfocontact/${userId}`);
-
+  const res = await axios.get(`${API_URL}/getdetailsInfocontact/${userId}`);
   return res.data;
-
 };
